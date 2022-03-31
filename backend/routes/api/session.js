e228c89b940944dc8591bf5da8b6447be6aadf87
Kateryna <tsymbal.katena@mail.ru>
2022-03-31 18:18:58 -0500

    Validation for login done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,16 +3,30 @@ const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation')
 
 
 const router = express.Router();
 
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a valid rassword '),
+    handleValidationErrors
 
+]
+
+// login
 router.post(
-    '/',
+    '/', validateLogin,
     asyncHandler(async (req, res, next) => {
         const { credential, password } = req.body;
-        console.log(credential, "<- credential ", password, "<- password")
+
 
         const user = await User.login({ credential, password });
 
