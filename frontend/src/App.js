b0c6f79d5b3a1155ff33ad3a3003bc8e8ebf7551
Kateryna <tsymbal.katena@mail.ru>
2022-04-03 21:32:45 -0500

    Model box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,27 @@
-import { useDispatch } from 'react-redux';
-import { useState, useEffect } from 'react';
-import React from 'react';
-import { Route, Switch, NavLink } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage';
-import SignupFormPage from './components/SignupFormPage'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import SignupFormPage from "./components/SignupFormModel";
 import * as sessionActions from "./store/session";
-import './index.css'
+import Navigation from "./components/Navigation";
 
 function App() {
-
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
-
   }, [dispatch]);
 
   return (
     <>
-      <nav>
-        <div className='navBar'>
-          <NavLink to="/login" className='navLink'>Login</NavLink>
-          <NavLink to="/signup" className='navLink'>SignUp</NavLink>
-        </div>
-      </nav>
-      <Switch>
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
-      </Switch>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
     </>
   );
 }
---
+++ frontend/src/components/SignupFormModel/index.js
@@ -0,0 +1,21 @@
+
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import SignupFormPage from './SignupForm';
+
+function SignupFormModal() {
+    const [showModal, setShowModal] = useState(false);
+
+    return (
+        <>
+            <button onClick={() => setShowModal(true)} className="loginButt">Sign Up</button>
+            {showModal && (
+                <Modal onClose={() => setShowModal(false)}>
+                    <SignupFormPage />
+                </Modal>
+            )}
+        </>
+    );
+}
+
+export default SignupFormModal;
